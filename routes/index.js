783af1d1713ab26a7d2c418d1460601a859551d7
Mehfuz Hossain <mehfuz@gmail.com>
2016-03-14 15:38:53 -0700

    fixes

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,13 @@ module.exports = function(app){
   app.post('/api/echo', function(req, res){
     var body = req.body;
 
-    console.log(body);
-    console.log(body.major);
 
+    app.socket.emit("message", body);
 
-    if (body.major > 1){
-        app.socket.emit("message", body);
-    }
     res.json ({
       success : true
     });
+
   });
 
   app.get('/echo', function(req, res){
